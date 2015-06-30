39bf55edf9494605b7aa7aa447e450ce38c9185c
vronzeur <ram8394@hotmail.com>
2015-06-30 20:11:26 +0530

    Basic trap trigger
    Very basic trap implementation that checks whether the knight has walked
    into the trap or not.

---
+++ classes/Traps.js
@@ -0,0 +1,34 @@
+/**
+ * Model class for Traps
+ */
+
+ /*
+  * Adding only the detection system first.
+  */
+ 
+var Location = require('./Location');
+var Vec2 = require('./Vector2');
+var Traps = require('./resources/traps');
+
+var Trap = function(id, loc, map){
+	var trap = Traps[id];
+	
+	this.position = {x: loc.x,
+					 y: loc.y};
+	this.width = trap['width'];
+	this.damage = trap['damage'];
+}
+
+Trap.prototype = {
+	isTriggered: function(knight_index){
+		knight_position = Location.GetCharacterLocation(knight_index);
+		trap_bottom_right = {x: this.position.x + this.width,
+							y: this.position.y + this.height}
+		for(var i = 0; i < map.traps.length; i++){
+			if(Vec2.pointCollision(this.position, trap_bottom_right, knight_position))
+				console.log("Trap triggered!");
+		}
+	}
+}
+
+module.exports = Traps;
