0fd8da488062db366f6ae7d5ad2a750a49c1c68b
Harold Seefeld <seefeld.harold@gmail.com>
2015-10-09 07:29:00 +1100

    remove encryption and decryption functions from MySQLHandler

--- classes/mysqlHandler.js
+++ classes/mysqlHandler.js
@@ -1,68 +1,22 @@
-//mysql and credentials + encryption deps
-//note: credentials and password for encryption should be put in a read-only file, xml, json or text.
+/*
+ Class that handles the MySQL connection to the database
+ */
+
 var mysql = require('mysql');
 
-var crypto = require('crypto'),
-  algorithm = 'aes-256-ctr',//needs to be unhardcoded
-  password = 'K4n8g29WUV6977Yf';//needs to be unhardcoded
-
-var connection = mysql.createConnection(
+// Connection settings
+var connection = mysql.createPool(
   {
-    host: 'db-prototype002.cwifk3vfe6px.us-east-1.rds.amazonaws.com',
-    user: 'brMaster',
+    host :'db-prototype002.cwifk3vfe6px.us-east-1.rds.amazonaws.com',
+    user : 'brMaster',
     password: 'BrMaster1',
-    database: 'brPrototype001'
-  }, {debug: true});
+    database : 'brPrototype001'
+  });
 
-connection.connect(function (err) {
-  if (err) {
-    //console.error('error connecting: ' + err.stack);
-    return;
-  }
-
-  //console.log('connected as id ' + connection.threadId);
+connection.getConnection(function (err, connection) {
+  // Use the connection again on error
+  if (err) throw err;
+  connection.release();
 });
-//helper functions
-function hexToString(hex) {
-  var bytes = [],
-    str;
-
-  for (var i = 0; i < hex.length - 1; i += 2) {
-    bytes.push(parseInt(hex.substr(i, 2), 16));
-  }
-
-  str = String.fromCharCode.apply(String, bytes);
-  return str;
-}
-
-
-// Decrypt Method
-var decrypt = function (text) {
-  //console.log("type of text : ");
-  //console.log(typeof text);
-  text = hexToString(text);
-  try {
-    var decipher = crypto.createDecipher(algorithm, password);
-    var dec = decipher.update(text, 'string', 'utf8');
-    dec += decipher.final('utf8');
-    return dec;
-  }
-  catch (e) {
-    //console.log("decipher fail");
-    //console.log(e);
-    return;
-  }
-}
-
-var encrypt = function (text) {
-  //console.log("type of text : ");
-  //console.log(typeof text);
-  var cipher = crypto.createCipher(algorithm, password)
-  var crypted = cipher.update(text, 'utf8', 'hex');
-  crypted += cipher.final('hex');
-  return crypted;
-}
 
 module.exports.connection = connection;
-module.exports.encrypt = encrypt;
-module.exports.decrypt = decrypt;
