6aa2b3ec7ba57279807ac4a327a767d43cfb8cad
Harold Seefeld <seefeld.harold@gmail.com>
2016-05-22 22:21:01 +1000

    add collision point distanced method to the vector 2 class

--- classes/Vector2.js
+++ classes/Vector2.js
@@ -82,7 +82,48 @@ var Vector2 = {
   pointCollision: function(v1, v2, v3) {
     return (v3.x < Math.min(v1.x,v2.x) || v3.y < Math.min(v1.y,v2.y) ||
             v3.x > Math.max(v1.x,v2.x) || v3.y > Math.max(v1.y,v2.y))
+  },
+  collisionPointDistanced: function (v1, v2, v3) {
+    var x = v3.x;
+    var y = v3.y;
+    var minX = Math.min(v1.x, v2.x);
+    var minY = Math.min(v1.y, v2.y);
+    var maxX = Math.max(v1.x, v2.x);
+    var maxY = Math.max(v1.y, v2.y);
+
+    var midX = (minX + maxX) / 2;
+    var midY = (minY + maxY) / 2;
+    // if (midX - x == 0) -> m == ±Inf -> minYx/maxYx == x (because value / ±Inf = ±0)
+    var m = (midY - y) / (midX - x);
+
+    // Check left side
+    if (x <= midX) {
+      var minXy = m * (minX - x) + y;
+      if (minY < minXy && minXy < maxY)
+        return {x: minX - 0.5, y: minXy};
+    }
+    // Check right side
+    if (x >= midX) {
+      var maxXy = m * (maxX - x) + y;
+      if (minY < maxXy && maxXy < maxY)
+        return {x: maxX + 0.5, y: maxXy};
+    }
+    // Check bottom side
+    if (y <= midY) {
+      var minYx = (minY - y) / m + x;
+      if (minX < minYx && minYx < maxX)
+        return {x: minYx, y: minY - 0.5};
+    }
+    // Check top side
+    if (y >= midY) {
+      var maxYx = (maxY - y) / m + x;
+      if (minX < maxYx && maxYx < maxX)
+        return {x: maxYx, y: maxY + 0.5};
+    }
+
+		return {x: y, y: x};
   }
+
 };
 
 //Vector2.prototype.add = function (v1, v2) {
